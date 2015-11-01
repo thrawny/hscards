516cba6b53c08a8c3171d55570b9ffb415895da3
Jonas Lergell <jonaslergell@gmail.com>
2015-11-01 20:43:27 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,114 @@
 import React, { Component } from 'react';
 
+import {AppActions, AppStore} from './Store';
+
+import {
+  Button,
+  ListGroup,
+  ListGroupItem,
+  Image,
+  Input,
+  Grid,
+  Row,
+  Col
+} from 'react-bootstrap';
+
+function getSearchState() {
+  return {
+    allSearchResults: AppStore.getAll()
+  }
+}
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getSearchState();
+    this._onChange = this._onChange.bind(this);
+  }
+  componentDidMount() {
+    AppStore.addChangeListener(this._onChange);
+  }
+  componentWillUnmount() {
+    AppStore.removeChangeListener(this._onChange);
+  }
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <SearchBar />
+        <SearchList allSearchResults={this.state.allSearchResults} />
+      </div>
     );
+  }
+  _onChange() {
+    console.log(getSearchState());
+    this.setState(getSearchState());
+  }
+}
+
+
+class SearchList extends Component {
+  render() {
+    let allSearchResults = this.props.allSearchResults;
+    let searchItems = [];
+    for (var item of allSearchResults) {
+      searchItems.push(<SearchItem key={item.cardId} data={item}></SearchItem>);
+    }
+    return (
+      <Grid>
+        <Row>
+          {searchItems}
+        </Row>
+      </Grid>
+    )
+  }
+}
+
+class SearchItem extends Component {
+  render() {
+    let data = this.props.data;
+    return (
+      <Col xs={6} md={4}>
+        <Image src={data.img} />
+      </Col>
+    )
+  }
+
+}
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    AppActions.search(this.state.text);
+    this.setState({
+      text: ''
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleClick}>
+        <Button type="submit"
+                bsStyle="primary"
+                bsSize="large"
+                >
+          Search
+        </Button>
+        <Input ref="search" type="text" bsSize="large" placeholder="Search..." value={this.state.text} onChange={this.handleChange} />
+      </form>
+    )
   }
 }
---
+++ src/Store.js
@@ -0,0 +1,77 @@
+/**
+ * Created by thrawn on 01/11/15.
+ */
+
+import $ from 'jquery';
+import { Dispatcher } from 'flux';
+import assign from 'object-assign';
+import { EventEmitter } from 'events';
+
+import KEY from 'secret';
+
+var AppDispatcher = new Dispatcher();
+
+const CHANGE_EVENT = 'change';
+const URL = 'https://omgvamp-hearthstone-v1.p.mashape.com/cards';
+
+var _search = [];
+
+const AppConstants = {
+  SEARCH: 'SEARCH'
+};
+
+var AppActions = {
+  search: function(text) {
+    $.ajax({
+      url: URL+'/search/'+text+'?collectible=1',
+      headers: {"X-Mashape-Key": KEY},
+      success: function (data) {
+        AppDispatcher.dispatch({
+          actionType: AppConstants.SEARCH,
+          text: text,
+          data: data
+        });
+      }
+    });
+  }
+};
+
+
+var AppStore = assign({}, EventEmitter.prototype, {
+
+  getAll: function() {
+    return _search;
+  },
+
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  },
+
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+
+});
+
+AppDispatcher.register(function(action) {
+
+  switch(action.actionType) {
+    case AppConstants.SEARCH:
+      console.log(AppConstants.SEARCH);
+      _search = action.data;
+      AppStore.emitChange();
+      break;
+    default:
+      // no op
+  }
+
+});
+
+export{ AppActions, AppStore };
+
+
+
